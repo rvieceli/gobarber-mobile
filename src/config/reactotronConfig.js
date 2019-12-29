6982acdef2bd89093829158fe571ac116aa8c6e3
Rafael Ricardo Vieceli <rafaelricardo3@gmail.com>
2019-12-29 19:19:59 +0100

    Configured redux and saga

--- src/config/reactotronConfig.js
+++ src/config/reactotronConfig.js
@@ -4,7 +4,7 @@ import reactotronSaga from 'reactotron-redux-saga';
 import AsyncStorage from '@react-native-community/async-storage';
 
 if (__DEV__) {
-  const tron = Reactotron.configure({ host: '172.20.10.2' })
+  const tron = Reactotron.configure({ host: '10.0.2.2' })
     .setAsyncStorageHandler(AsyncStorage)
     .useReactNative()
     .use(reactotronRedux())
