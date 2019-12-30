3cbfd818c4cda2c7398f9c309023a5d7cf65644d
Rafael Ricardo Vieceli <rafaelricardo3@gmail.com>
2019-12-30 18:19:37 +0100

    SelectDateTime page is done!

--- src/pages/New/SelectDateTime/index.js
+++ src/pages/New/SelectDateTime/index.js
@@ -1,15 +1,54 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
+import api from '~/services/api';
 import { Background, IconButton, DateInput } from '~/components';
-import { Container } from './styles';
 
-export default function SelectDateTime() {
+import { Container, List, Hour, Title } from './styles';
+
+export default function SelectDateTime({ navigation }) {
   const [date, setDate] = useState(new Date());
+  const [hours, setHours] = useState([]);
+
+  const provider = navigation.getParam('provider');
+
+  useEffect(() => {
+    async function loadAvailableHours() {
+      const { data } = await api.get(`providers/${provider.id}/available`, {
+        params: {
+          date: date.getTime(),
+        },
+      });
+
+      setHours(data);
+    }
+
+    loadAvailableHours();
+  }, [date, provider.id]);
+
+  function handleSelectHour(time) {
+    navigation.navigate('Confirm', {
+      provider,
+      time,
+    });
+  }
 
   return (
     <Background>
       <Container>
         <DateInput date={date} onChange={setDate} />
+
+        <List
+          data={hours}
+          keyExtractor={hour => hour.time}
+          renderItem={({ item: hour }) => (
+            <Hour
+              enabled={hour.available}
+              onPress={() => handleSelectHour(hour.value)}
+            >
+              <Title>{hour.time}</Title>
+            </Hour>
+          )}
+        />
       </Container>
     </Background>
   );
